32843e4eb924dc82f1b2dd1109722d8435a2962f
mariolsun <mariolsun@yandex.ru>
2022-06-12 19:35:57 +0300

    hid safari ios address bar

--- script/index.js
+++ script/index.js
@@ -260,7 +260,14 @@ if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(naviga
   })
 }
 
-
+if (navigator.userAgent.match(/iPhone/i) || navigator.userAgent.match(/iPad/i)) { 
+  /* iOS hides Safari address bar */
+  window.addEventListener("load",function() {
+      setTimeout(function() {
+          window.scrollTo(0, 1);
+      }, 1000);
+  });
+}
 
 
 /*
